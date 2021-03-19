006cd63fc644cc58f3786560f75eaa7f8818efec
Sabri Bouchaala <bouchaala.sabri@gmail.com>
2021-03-19 12:58:17 +0100

    Simplify code

--- server/top_albums.py
+++ server/top_albums.py
@@ -5,20 +5,5 @@ import falcon
 class TopAlbums(object):
 
     def on_get(self, req, resp):
-        albums = []
-
         chart = billboard.ChartData('rap-albums')
-
-        for entry in chart:
-            album = {}
-            album['title'] = entry.title
-            album['image']  = entry.image
-            albums.append(album)
-
-        # Create a JSON representation of the resource
-        resp.body = json.dumps(albums, ensure_ascii=False)
-
-        # The following line can be omitted because 200 is the default
-        # status returned by the framework, but it is included here to
-        # illustrate how this may be overridden as needed.
-        resp.status = falcon.HTTP_200
+        resp.body = chart.json()
